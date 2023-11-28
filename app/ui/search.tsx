8d584162baaa7c9cc6f363c8c85df18905810171
AndyM <marsden.a@gmail.com>
2023-11-28 12:21:30 +0000

    pagination, set page num on new search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
         // assign a URLSearchParams object instant to params
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
 
         if (term) {
             params.set('query', term);
